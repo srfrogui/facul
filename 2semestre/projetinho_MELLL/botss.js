61cd013194a32e4d9c6d9c609623cf935c3695aa
Frog <omarrenardo33801@gmail.com>
2023-08-13 19:07:24 -0300

    organizando codigo

--- 2semestre/projetinho_MELLL/botss.js
+++ 2semestre/projetinho_MELLL/botss.js
@@ -1,31 +1,133 @@
-const { Client } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 const mysql = require('mysql');
-const client = new Client();
+const fs = require('fs');
+
+const SESSION_FILE_PATH = './session.json'; // Caminho para o arquivo de sessão
+
+let sessionData;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+  sessionData = require(SESSION_FILE_PATH);
+}
+
+const client = new Client({
+  puppeteer: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  },
+  session: sessionData,
+  authStrategy: new LocalAuth({ session: sessionData })
+});
 
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'omar',
-  password: 'Fantasma01',
-  database: 'melado'
+  user: 'root',
+  password: '',
+  database: 'test'
 });
 
+// Defina os preços unitários para cada tipo e tamanho de mel
+const precosUnitarios = {
+  'lar': {
+    270: 11.00,
+    450: 18.00,
+    500: 19.00,
+    800: 30.00
+  },
+  'sil': {
+    270: 10.00,
+    450: 16.00,
+    500: 17.00,
+    800: 26.00
+  },
+  'euc': {
+    270: 10.00,
+    450: 16.00,
+    500: 17.00,
+    800: 26.00
+  }
+};
+
+// Função para mapear tipos de mel para opções abreviadas
+function mapTipoMel(tipo) {
+  const tipoMelMap = {
+    'lar': 'lar',
+    'laranjeira': 'lar',
+    'sil': 'sil',
+    'silvestre': 'sil',
+    'euc': 'euc',
+    'eucalipto': 'euc'
+  };
+  return tipoMelMap[tipo.toLowerCase()] || null;
+}
+
+// Função para verificar se um pedido existe
+function checkPedidoExistente(pessoa, quantidade, tipoMel, tamanho) {
+  return new Promise((resolve, reject) => {
+    const selectQuery = `SELECT * FROM pedidos WHERE pessoa = ? AND quantidade = ? AND tipo = ? AND tamanho = ?`;
+    db.query(selectQuery, [pessoa, quantidade, tipoMel, tamanho], (err, results) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(results.length > 0);
+    });
+  });
+}
+
+// Função para calcular o preço unitário do mel com base no tipo e tamanho
+function calcularPrecoUnitario(tipoMel, tamanho) {
+  const tipo = mapTipoMel(tipoMel);
+  if (tipo && precosUnitarios[tipo] && precosUnitarios[tipo][tamanho]) {
+    return precosUnitarios[tipo][tamanho];
+  } else {
+    return 0; // Caso os valores não sejam encontrados
+  }
+}
+
+// Função para calcular o total dos pedidos de uma pessoa
+function calcularTotalPedidos(pedidos) {
+  let total = 0;
+  for (const pedido of pedidos) {
+    const precoUnitario = calcularPrecoUnitario(pedido.tipo, pedido.tamanho);
+    total += pedido.quantidade * precoUnitario;
+  }
+  return total;
+}
+
+// Conexão ao banco de dados
 db.connect((err) => {
   if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
-    return;
-  }
+      console.error('Erro ao conectar ao banco de dados:', err);
+      return;
+    }
   console.log('Conexão bem-sucedida ao banco de dados!');
 });
 
-client.on('qr', (qr) => {
-  console.log('QR Code:', qr);
+// Exibir código QR
+client.on('qr', qr => {
+  qrcode.generate(qr, { small: true });
 });
 
+// Cliente pronto
 client.on('ready', () => {
   console.log('Bot está pronto!');
 });
 
+// Autenticação bem-sucedida
+client.on('authenticated', (session) => {
+  console.log('Cliente autenticado');
+  // Salva a sessão em um arquivo para restauração futura
+  fs.writeFileSync(SESSION_FILE_PATH, JSON.stringify(session));
+});
+
+// Falha na autenticação
+client.on('auth_failure', (error) => {
+  console.error('Falha na autenticação:', error);
+  // Lida com a falha na autenticação, se necessário
+});
+
 client.initialize();
+
 
 client.on('message', async (message) => {
   const parts = message.body.split(' ');
@@ -69,8 +171,7 @@ client.on('message', async (message) => {
     });
   }
 
-
-  if (parts[0].startsWith('!cancelar') && parts.length === 5) {
+  if (parts[0].startsWith('!cancelar') && parts.length === 4) {
     const pessoaPedido = parts[1];
     const quantidade = parseInt(parts[2]);
     const tipoMel = mapTipoMel(parts[3]);
@@ -203,73 +304,15 @@ client.on('message', async (message) => {
       message.reply('Comando !status inválido. Use !status {nome pessoa} para mostrar pedidos e calcular total de uma pessoa ou !status [lar|sil|euc] para filtrar por tipo de mel.');
     }
   }
+  
+  if (parts[0].startsWith('!receba') && parts.length >= 1) {
+    message.reply(`
+      VALE FOTOTETA
+   
+  ╭╴╴╴╴╴✁ ╴╴╴╴╴╴╮ 
+  ┆      P E I T I N H O       ┆ 
+  ╰╴╴╴╴✃ ╴╴╴╴╴╴╴╯
+    `);
+  }
 });
 
-// Função para mapear tipos de mel para opções abreviadas
-function mapTipoMel(tipo) {
-  const tipoMelMap = {
-    'lar': 'lar',
-    'laranjeira': 'lar',
-    'sil': 'sil',
-    'silvestre': 'sil',
-    'euc': 'euc',
-    'eucalipto': 'euc'
-  };
-  return tipoMelMap[tipo.toLowerCase()] || null;
-}
-
-// Defina os preços unitários para cada tipo e tamanho de mel
-const precosUnitarios = {
-  'lar': {
-    270: 10.00,
-    500: 18.00,
-    450: 16.00,
-    800: 28.00
-  },
-  'sil': {
-    270: 12.00,
-    500: 20.00,
-    450: 18.00,
-    800: 30.00
-  },
-  'euc': {
-    270: 15.00,
-    500: 25.00,
-    450: 22.00,
-    800: 35.00
-  }
-};
-
-// Função para verificar se um pedido existe
-function checkPedidoExistente(pessoa, quantidade, tipoMel, tamanho) {
-  return new Promise((resolve, reject) => {
-    const selectQuery = `SELECT * FROM pedidos WHERE pessoa = ? AND quantidade = ? AND tipo = ? AND tamanho = ?`;
-    db.query(selectQuery, [pessoa, quantidade, tipoMel, tamanho], (err, results) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve(results.length > 0);
-    });
-  });
-}
-
-// Função para calcular o preço unitário do mel com base no tipo e tamanho
-function calcularPrecoUnitario(tipoMel, tamanho) {
-  const tipo = mapTipoMel(tipoMel);
-  if (tipo && precosUnitarios[tipo] && precosUnitarios[tipo][tamanho]) {
-    return precosUnitarios[tipo][tamanho];
-  } else {
-    return 0; // Caso os valores não sejam encontrados
-  }
-}
-
-// Função para calcular o total dos pedidos de uma pessoa
-function calcularTotalPedidos(pedidos) {
-  let total = 0;
-  for (const pedido of pedidos) {
-    const precoUnitario = calcularPrecoUnitario(pedido.tipo, pedido.tamanho);
-    total += pedido.quantidade * precoUnitario;
-  }
-  return total;
-}
